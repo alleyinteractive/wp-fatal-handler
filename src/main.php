a1996554c8522b20a224323ebd775c3a1c825ff7
Sean Fisher <srtfisher@gmail.com>
2025-06-25 12:00:33 -0400

    Using whoops instead

--- src/main.php
+++ src/main.php
@@ -7,22 +7,70 @@
 
 namespace Alley\WP\WP_Fatal_Error_Handler;
 
-use ErrorException;
-use Symfony\Component\ErrorHandler\ErrorRenderer\HtmlErrorRenderer;
+use Whoops\Handler\Handler;
+use Whoops\Run as Whoops;
+use Whoops\Handler\JsonResponseHandler;
+use Whoops\Handler\PlainTextHandler;
+use Whoops\Handler\PrettyPageHandler;
 
 /**
  * Instantiate the plugin.
  */
 function main(): void {
-	add_filter( 'wp_php_error_message', __NAMESPACE__ . '\handle_wp_php_error_message', 10, 2 );
+	if ( ! should_register_handler() ) {
+		return;
+	}
+
+	$handler = match ( true ) {
+		str_contains( $_SERVER['REQUEST_URI'] ?? '', '/wp-json/' ) => new JsonResponseHandler(),
+		// Check if the request expects JSON.
+		str_contains( $_SERVER['HTTP_ACCEPT'] ?? '', '/json' ) => new JsonResponseHandler(),
+		defined( 'WP_CLI' ) && WP_CLI => new PlainTextHandler(),
+		default => new PrettyPageHandler(),
+	};
+
+	$handler = apply_filters( 'wp_fatal_handler_whoops_handler', $handler );
+
+	// If the handler is not an instance of Handler, default to
+	// PrettyPageHandler. To unregister whoops error handling, use the
+	// `wp_fatal_handler_whoops` filter to return `null`.
+	if ( ! $handler instanceof Handler ) {
+		$handler = new PrettyPageHandler();
+	}
+
+	$whoops  = new Whoops();
+	$whoops->pushHandler( $handler );
+
+	// Ignore non-fatal errors (warnings, notices, etc.).
+	$whoops->silenceErrorsInPaths(
+		// Ignore all non-fatal errors in WordPress.
+		'/^' . preg_quote( ABSPATH, '/' ) . '.*/',
+		E_WARNING | E_NOTICE | E_DEPRECATED | E_USER_DEPRECATED,
+	);
+
+	/**
+	 * Filter the Whoops instance before it is registered.
+	 *
+	 * @param \Whoops\Run $whoops The Whoops instance.
+	 */
+	$whoops = apply_filters( 'wp_fatal_handler_whoops', $whoops );
+
+	if ( $whoops instanceof Whoops ) {
+		$whoops->register();
+	}
 }
 
 /**
- * Determine if the fatal error handler should intercept errors.
+ * Determine whether the fatal error handler should be registered.
  *
- * @return bool True if the fatal error handler should intercept errors, false otherwise.
+ * This function checks if the `WP_DEBUG` constant is defined and set to true,
+ * and if the environment is set to 'local' in the `VIP_GO_APP_ENVIRONMENT
+ * constant`. It also allows for filtering the registration
+ * through the `wp_fatal_handler_register` filter.
+ *
+ * @return bool
  */
-function should_intercept_error(): bool {
+function should_register_handler(): bool {
 	if ( ! defined( 'WP_DEBUG' ) || ! WP_DEBUG ) {
 		return false;
 	}
@@ -31,47 +79,5 @@ function should_intercept_error(): bool {
 		return false;
 	}
 
-	return (bool) apply_filters( 'wp_fatal_handler_intercept_error', true );
-}
-
-/**
- * Handle the `wp_php_error_message` filter.
- *
- * @param string $message Default error message.
- * @param array  $error Error details.
- * @phpstan-param array{type?: int, message?: string, file?: string, line?: int} $error
- * @return string|never
- */
-function handle_wp_php_error_message( string $message, array $error ): mixed {
-	if ( ! should_intercept_error() ) {
-		return $message;
-	}
-
-	if ( ! class_exists( HtmlErrorRenderer::class ) ) {
-		_doing_it_wrong(
-			'wp_fatal_handler',
-			'The HtmlErrorRenderer class is not available. Please ensure the Symfony ErrorHandler component is installed.', // phpcs:ignore
-			'0.1.0'
-		);
-
-		return $message;
-	}
-
-	$instance = new HtmlErrorRenderer(
-		debug: true,
-		charset: get_bloginfo( 'charset', 'display' ),
-		projectDir: ABSPATH,
-	);
-
-	$exception = new ErrorException(
-		$error['message'] ?? $message,
-		0,
-		$error['type'] ?? E_ERROR,
-		$error['file'] ?? '',
-		$error['line'] ?? 0,
-	);
-
-	echo $instance->render( $exception )->getAsString(); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-
-	exit( 1 );
+	return (bool) apply_filters( 'wp_fatal_handler_register', true );
 }
